32e9e955a2e6c49d9733b7cf0bacd2c99ca36052
Jaydex-69 <jawadtariq600@gmail.com>
2026-04-17 13:19:58 -0700

    frontend

--- Current/flask app/app.py
+++ Current/flask app/app.py
@@ -1,21 +1,19 @@
-from flask import Flask, jsonify, request, render_template
-from pathlib import Path
+from flask import Flask, jsonify, request
 from utils import predict_class, get_response
+from flask_cors import CORS
 
-folder = Path(__file__).parent/'templates'
+app = Flask(__name__)
+CORS(app)  # IMPORTANT for React
 
-app = Flask(__name__, template_folder=folder)
-
-app.route('/')
-def index():
-    return render_template('index.html')
-
-app.route('handle_message', method=['POST'])
+@app.route('/chat', methods=['POST'])
 def handle_message():
-    message = request.json['message']
+    data = request.json
+    message = data.get('message')
+
     prediction = predict_class(message)
     response = get_response(prediction)
 
     return jsonify({'response': response})
 
-app.run('0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
